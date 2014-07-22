ae78b91d920b71b2df48af004dd84becaa562193
Brian Owen <brian@brianowen.net>
2014-07-22 10:25:46 -0700

    Updated to .js

---
+++ js/chartDefaults.js
@@ -0,0 +1,11 @@
+var properties = {
+									surgeryColor: 'rgba(220, 20, 50, .7)',
+									seedsColor: 'rgba(20, 20, 220, .7)',
+									ebrtColor: 'rgba(20, 100, 20, .7)',
+									hdrColor: 'rgba(20, 120, 230, .7)',
+									protonColor: 'rgba(200, 200, 30, .7)',
+									hifuColor: 'rgba(20, 22, 25, .7)',
+									cryoColor: 'rgba(160, 30, 250, .7)',
+									
+									yText: 'Treatment Success <br> % PSA Progression Free'
+}
